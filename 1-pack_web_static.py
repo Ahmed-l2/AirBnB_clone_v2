44bea5d4d840e434cde0324e4edd65a9d85526e2
Ahmed <ahmed_l2@yahoo.com>
2024-04-04 20:42:56 +0300

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 def do_pack():
-    """"""
+    """generates a .tgz archive from the contents of the web_static"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
 
     try:
