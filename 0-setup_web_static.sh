972fb812f0e86ae9ee35a705dc544ef818c3c37a
Ahmed <ahmed_l2@yahoo.com>
2024-04-04 04:19:37 +0300

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -29,7 +29,15 @@ mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/releases/test/
 
 # Simple HTML file for testing
-echo "Hello, this is a test page." > /data/web_static/releases/test/index.html
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
 
 # Create or recreate the symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
