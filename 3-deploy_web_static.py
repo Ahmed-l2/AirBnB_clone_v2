3b3fe3e1ea0906c1c0e748623ff9ecdea4906e86
Ahmed <ahmed_l2@yahoo.com>
2024-04-05 04:27:20 +0300

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -46,7 +46,7 @@ def do_deploy(archive_path):
 def deploy():
     """"creates and distributes an archive to your web servers"""
     archive_path = do_pack()
-    if not archive_path or not os.path.exists(archive_path):
+    if archive_path is None:
         return False
-
-    return (do_deploy(archive_path))
+    result = do_deploy(archive_path)
+    return (result)
