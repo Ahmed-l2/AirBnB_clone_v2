6109e32e55d0f07868346bebaaa6fc7410c7e2be
Ahmed <ahmed_l2@yahoo.com>
2024-04-19 21:30:03 +0300

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""FLASK APP"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greet():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pye(text):
+    if not text:
+        text = "is cool"
+
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
