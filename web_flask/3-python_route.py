28e1829b2e11ddc285c4ab9ba46f36de896ae575
Ahmed <ahmed_l2@yahoo.com>
2024-04-19 21:38:07 +0300

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,11 +21,9 @@ def c(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def pye(text):
-    if not text:
-        text = "is cool"
-
+def pye(text="is_cool"):
     return "Python {}".format(text.replace("_", " "))
 
 
