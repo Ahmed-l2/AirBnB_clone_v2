da3113a23934e48ae90e227b62f6626e2f67a187
Ahmed <ahmed_l2@yahoo.com>
2024-04-20 18:48:20 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,58 +2,21 @@
 """FLASK APP"""
 
 from models import storage
-from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def greet():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pye(text="is_cool"):
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def num(n):
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def num_h1(n):
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_even(n):
-    return render_template("6-number_odd_or_even.html", n=n,
-                           eo='even' if n % 2 == 0 else 'odd')
+@app.teardown_appcontext
+def teardown(exception=None):
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    states = sorted(list(storage.all(State).values()), key=lambda s: s.name)
+    states = list(storage.all("State").values())
+    states.sort(key=lambda s: s.name)
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
 
 
 if __name__ == "__main__":
