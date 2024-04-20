f2a29c764dda532fcbf5d0e31ea4376e4e0838b0
Ahmed <ahmed_l2@yahoo.com>
2024-04-20 22:11:37 +0300

    minor fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,12 +19,13 @@ def state_list():
     return render_template("7-states_list.html",
                            states=storage.all(State))
 
+
 @app.route("/states/<id>", strict_slashes=False)
 def state_by_id(id):
     for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html",
-                           states=state)
+                                   states=state)
     return render_template("9-states.html", states=None)
 
 
