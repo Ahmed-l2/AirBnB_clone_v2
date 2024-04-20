5f984ff94b817c40bf7153bd187b7d9185c5f1f3
Ahmed <ahmed_l2@yahoo.com>
2024-04-20 18:44:39 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -47,7 +47,7 @@ def odd_even(n):
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    states = sorted(list(storage.all("State").values()), key=lambda s: s.name)
+    states = sorted(list(storage.all(State).values()), key=lambda s: s.name)
     return render_template("7-states_list.html", states=states)
 
 
