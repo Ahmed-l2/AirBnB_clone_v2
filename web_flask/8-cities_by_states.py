0871bbd28fdbf3b85a8f7d40adb4a83b791b7fd8
Ahmed <ahmed_l2@yahoo.com>
2024-04-20 20:32:52 +0300

    minor fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,7 +17,8 @@ def teardown(exception):
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states_list():
     states = storage.all(State)
-    return render_template("8-cities_by_states.html", states=storage.all(State))
+    return render_template("8-cities_by_states.html",
+                           states=storage.all(State))
 
 
 if __name__ == "__main__":
